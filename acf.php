bcc419c32df5d865455c9d9a3743a07ba4db2e2e
Ivan Mattie <mattieivan@gmail.com>
2018-02-05 14:37:10 -0700

    Basic template created

---
+++ acf.php
@@ -0,0 +1,12 @@
+<?php
+function mwpm_examples_example_module_generate() {
+    return [
+        'key' => '5a78cbf896799',
+        'name' => 'example_module',
+        'label' => 'Example Module',
+        'display' => 'block',
+        'sub_fields' => [],
+        'min' => '',
+        'max' => ''
+    ];
+}
---
+++ template.php
@@ -0,0 +1,3 @@
+<section class="example-module content-module">
+    This is an example module.
+</section>
