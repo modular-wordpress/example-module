9376acf01dfc7d0ae68a4ebfe09302f999b6b9ec
Ivan Mattie <mattieivan@gmail.com>
2018-02-06 10:19:12 -0700

    Switched up how ACF registration works

--- acf.php
+++ acf.php
@@ -1,12 +1,11 @@
 <?php
-function mwpm_examples_example_module_generate() {
-    return [
-        'key' => '5a78cbf896799',
-        'name' => 'example_module',
-        'label' => 'Example Module',
-        'display' => 'block',
-        'sub_fields' => [],
-        'min' => '',
-        'max' => ''
-    ];
-}
+
+mwpm_register_module([
+    'key' => '5a78cbf896799',
+    'name' => 'example_module',
+    'label' => 'Example Module',
+    'display' => 'block',
+    'sub_fields' => [],
+    'min' => '',
+    'max' => ''
+]);
